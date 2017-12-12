8a5407e5ca1c3dfad748d66eee61d1ecc37b089e
james donlan and kyle tenzeldam <james@>
2017-12-12 11:29:39 -0800

    price now successfully returns

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 //business logic
-function Ticket(movie, time, age) {
+var price = 10;
+var matinee = 8;
+function Ticket(movie, time, age, price) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  this.price = price;
 }
-
+Ticket.prototype.finalprice = function() {
+  if (this.time === "12:00") {
+    var ticketPrice = matinee;
+  } else {
+    var ticketPrice = price;
+  }
+  if ((this.age === "youth") || (this.age === "senior")) {
+    ticketPrice = ticketPrice * 0.8;
+  }
+  return ticketPrice;
+}
 // user interface logic
 $(document).ready(function() {
 
@@ -15,9 +28,8 @@ $(document).ready(function() {
     var inputtedMovie = $("#movies").val();
     var inputtedTimes = $("#times").val();
     var inputtedAge = $("input:radio[name=age]:checked").val();
-
-    var newTicket = new Ticket(inputtedMovie, inputtedTimes, inputtedAge);
-debugger;
+    var newTicket = new Ticket(inputtedMovie, inputtedTimes, inputtedAge, price);
+    var finalPrice = newTicket.finalprice();
     $("ul#ticket").append("<li><span class='movieticket'>" + newTicket.movie + "</span></li>");
 
     $(".movieticket").last().click(function() {
@@ -26,7 +38,7 @@ debugger;
       $(".moviechoice").text(newTicket.movie);
       $(".timechoice").text(newTicket.time);
       $(".agegroup").text(newTicket.age);
-      // $(".price").text(Ticket.age);
+      $(".price").text(finalPrice);
 
         // $("#addresses").click(function(){
         //   $("li").remove();
