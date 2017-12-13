b2b2499edeb655a0b427b101ecb6e4a33516f370
james donlan and kyle tenzeldam <james@>
2017-12-12 16:25:24 -0800

    add js and allow balance to be seen

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,59 @@
 
+var total;
+
+function Account(name, balance) {
+this.name = name;
+this.balance = balance;
+}
+
+  Account.prototype.withdrawFunds = function(inputtedWithdrawal) {
+    this.balance = this.balance -= inputtedWithdrawal;
+    // return initial + inputtedDeposit;
+}
+
+Account.prototype.depositFunds = function(inputtedInitialdepost) {
+  this.balance = this.balance += inputtedInitialdepost;
+  // return initial + inputtedDeposit;
+}
+
+
+
+
+
+
+
 // user interface logic
 $(document).ready(function() {
 
-
-  $("form#movieform").submit(function(event) {
+  $("form.formaccount").submit(function(event) {
+    // debugger;
     event.preventDefault();
-
     var inputtedName = $("input#name").val();
-    var inputtedInitialdepost = $("input#initialdepost").val();
-    var inputtedDeposit = $("input#deposit").val();
-    var inputtedWithdrawal = $("input#withdrawal").val();
+    var balance = parseInt($("input#initialdepost").val());
+    var newAccount = new Account(inputtedName, balance);
 
-    $("span#balanceoutpu").text()
-      });
+
+    $("span#balanceoutput").text(newAccount.balance)
+    $("form.formDW").submit(function(event) {
+      event.preventDefault();
+      var inputtedDeposit = parseInt($("input#deposit").val());
+      var inputtedWithdrawal = parseInt($("input#withdrawal").val());
+console.log(newAccount.balance);
+
+      $("span#balanceoutput").text(newAccount.balance)
     });
+
+
   });
+});
+
+
+// function accountChange() {
+//   if ((inputtedDeposit === 0) && (inputtedWithdrawal !== 0)) {
+//     return inputtedWithdrawal * -1;
+//   } if ((inputtedDeposit !== 0) && (inputtedWithdrawal === 0)) {
+//     return inputtedDeposit;
+//   } else {
+//     return inputtedDeposit - inputtedWithdrawal;
+//   }
+// }
